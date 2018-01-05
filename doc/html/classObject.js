2802dd32b826d22e7a3275d885e55091c11b7dd5
Yt-trium <cyril.meyer.68@gmail.com>
2018-01-05 22:17:16 +0100

    update doc

--- doc/html/classObject.js
+++ doc/html/classObject.js
@@ -3,5 +3,6 @@ var classObject =
     [ "Object", "classObject.html#a40860402e64d8008fb42329df7097cdb", null ],
     [ "Object", "classObject.html#a30ff0733f6b70bc1037d58b927237a0c", null ],
     [ "draw", "classObject.html#ad9684af664b6b318174bb147d88131fe", null ],
+    [ "random", "classObject.html#ac28acd7323fc00d2522b22b19a10a7d6", null ],
     [ "ui", "classObject.html#a0a9d6c7d2325fc1eb7e5854151a51d09", null ]
 ];
--- doc/html/search/all_d.js
+++ doc/html/search/all_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['random',['random',['../classObject.html#ac28acd7323fc00d2522b22b19a10a7d6',1,'Object']]],
   ['randomize',['randomize',['../classTerrain.html#abc478a72ef7b4b9dae3911d1acee4c71',1,'Terrain::randomize()'],['../classTerrain.html#a9fa2515b7c84025c74c97af9b3fee30f',1,'Terrain::randomize(std::vector&lt; Vertex &gt; &amp;pPoints, unsigned int pSlicing)']]],
   ['reset',['reset',['../classCamera.html#aa7695a960438e5571e14f50ed69f6623',1,'Camera::reset()'],['../classFPSCamera.html#a5076d0c700255c33daff490d73c77761',1,'FPSCamera::reset()'],['../classOrbitCamera.html#a022751aa06693232844732adb00e7d71',1,'OrbitCamera::reset()']]]
 ];
